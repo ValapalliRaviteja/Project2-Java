81d469993b8ca0b10b9a82e489d0adcd93f1223e
ValapalliRaviteja <99816535+ValapalliRaviteja@users.noreply.github.com>
2022-03-08 14:46:25 +0530

    Add files via upload

---
+++ CHACHE.java
@@ -0,0 +1,60 @@
+package chache;
+
+import java.util.*;
+
+public class CHACHE {
+	LinkedHashMap<String,Integer> mapList=new LinkedHashMap<>();
+	public static List<String> list=new ArrayList<>();
+	
+
+	
+	
+	public void put(String str,int i) {
+		
+		if (this.mapList.size()>=3 && list.size()>=3) {
+			
+			this.mapList.remove(list.get(0));
+		    this.mapList.put(str, i);
+		    list.remove(0);
+		    list.add(str);
+		   
+		}
+		else {
+			list.add(str);
+			this.mapList.put(str, i);
+		}
+	}
+	
+	public int get(String str) {
+		   if (mapList.containsKey(str)) {
+		    int value=mapList.get(str);
+		    list.remove(str);
+		    list.add(str);
+		    this.mapList.remove(str);
+		    this.mapList.put(str, value);
+			
+		        return mapList.get(str);
+		   }
+		return -1;
+	}
+	
+	public  void getLRU() {
+		System.out.println(list.get(0));
+		
+		
+	}
+	
+		
+}	
+
+		
+	   
+		
+		
+	  
+		
+		
+	  
+	    
+		
+
